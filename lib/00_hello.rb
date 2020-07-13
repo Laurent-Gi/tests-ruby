37d0812f6f398bbaecd2f20f824b25032719d71d
Laurent Saives <etrenjeu@gmail.com>
2020-07-13 10:50:15 +0200

    Ajout de l'exercice 00_hello fichiers  .rb et _spec.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,34 @@
+# Méthode pour dire Bonjour !
 def hello
-
+  "Hello!"
 end
 
+# Méthode pour saluer quelqu'un en utilisant son prénom ou pseudo
 def greet(name)
 
+  return hello if name.to_s.empty?
+
+  # Si il y a un prénom ou pseudo, on l'utilise
+  "Hello, #{name}!"
+
 end
+
+# Petit programme 
+def ask_name
+  print "Quel est votre prénom ?\n> "
+  name = gets.chomp
+end
+
+
+def perform
+  name = ask_name
+  puts greet(name)
+end
+
+# -------------------------------------------------------
+
+perform 
+
+puts greet(1.278)
+puts greet(false)
+puts greet(17)
--- spec/00_hello_spec.rb
+++ spec/00_hello_spec.rb
@@ -6,6 +6,8 @@ describe "the hello function" do
   end
 end
 
+# Saluer quelq'un avec son prénom ou pseudo
+# -----------------------------------------
 describe "the greet function" do
   it "says hello to someone" do
     expect(greet("Alice")).to eq("Hello, Alice!")
@@ -14,4 +16,18 @@ describe "the greet function" do
   it "says hello to someone else" do
     expect(greet("Bob")).to eq("Hello, Bob!")
   end
+
+
+  it "says hello to not a float" do
+    expect(greet(1.278)).to eq("Hello, 1.278!")
+  end
+
+  it "says hello to not a boolean" do
+    expect(greet(false)).to eq("Hello, false!")
+  end
+
+  it "says hello to not an integer" do
+    expect(greet(17)).to eq("Hello, 17!")
+  end
+
 end
